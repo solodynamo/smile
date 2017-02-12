93e3a483fd01f9f552dd8a60f3682199bcb08f19
solodynamo <bittuf3@gmail.com>
2017-02-12 20:41:05 +0530

    Smiles added

---
+++ laugh.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var request = require('request');
+var chalk = require('chalk');
+
+program
+    .version('0.0.1')
+    .usage('Type smile')
+    .option('Just type smile ...and smile')
+    .parse(process.argv);
+
+if(!program.args.length) {
+    var url = 'http://api.icndb.com/jokes/random?firstName=Cyber&amp&&lastName=Rajni&&limitTo=nerdy';
+    request({
+    method: 'GET',
+    headers: {
+    },
+    url: url
+	}, function(error, response, body) {
+
+    if (!error && response.statusCode == 200) {
+        var body = JSON.parse(body);
+        console.log(chalk.green.bold.bgBlack(body.value.joke));
+    } else if (error) {
+         console.log(chalk.red('Error: ' + error));
+    }
+	});
+} else {
+    	console.log(chalk.magenta.bold.underline("No Arguments Plz, just smile :)"));
+}
+
