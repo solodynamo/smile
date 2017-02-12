f910e6ef61c7f55629cd5f82b56775d69e262ec8
solodynamo <bittuf3@gmail.com>
2017-02-12 20:51:38 +0530

    adds jshint

--- laugh.js
+++ laugh.js
@@ -10,23 +10,21 @@ program
     .option('Just type smile ...and smile')
     .parse(process.argv);
 
-if(!program.args.length) {
+if (!program.args.length) {
     var url = 'http://api.icndb.com/jokes/random?firstName=Cyber&amp&&lastName=Rajni&&limitTo=nerdy';
     request({
-    method: 'GET',
-    headers: {
-    },
-    url: url
-	}, function(error, response, body) {
+        method: 'GET',
+        headers: {},
+        url: url
+    }, function(error, response, body) {
 
-    if (!error && response.statusCode == 200) {
-        var body = JSON.parse(body);
-        console.log(chalk.green.bold.bgBlack(body.value.joke));
-    } else if (error) {
-         console.log(chalk.red('Error: ' + error));
-    }
-	});
+        if (!error && response.statusCode == 200) {
+            body = JSON.parse(body);
+            console.log(chalk.green.bold.bgBlack(body.value.joke));
+        } else if (error) {
+            console.log(chalk.red('Error: ' + error));
+        }
+    });
 } else {
-    	console.log(chalk.magenta.bold.underline("No Arguments Plz, just smile :)"));
+    console.log(chalk.magenta.bold.underline("No Arguments Plz, just smile :)"));
 }
-
